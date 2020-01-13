4ad8f652a3922e006fa9750cf46870ee25c7218b
Tu Nombre <mike.mavc27@gmail.com>
2020-01-13 14:58:11 -0300

    primer spring

---
+++ src/test/java/com/zeus/springboot/app/SpringZeusJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeus.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZeusJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
